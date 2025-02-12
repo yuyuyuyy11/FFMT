6b148b146c9cd85606d7bd47cc25966c20b37d35
月的高 <2738859020@qq.com>
2025-02-12 15:04:36 +0800

    update

--- models/build_model.py
+++ models/build_model.py
@@ -4,44 +4,41 @@ from .model import *
 
 
 def build_model(cfg):
-
-    if cfg.model == 'PCReg_KPURes18_MSF':
-        model = PCReg_KPURes18_MSF(cfg)
-    elif cfg.model == "no_model":
+    if cfg.model == "no_model":
         print("Warning: no model is being loaded; rarely correct thing to do")
         model = torch.nn.Identity()
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr':
-        model = PCReg_KPURes18_MSF_loftr(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr1':
-        model = PCReg_KPURes18_MSF_loftr1(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr2':
-        model = PCReg_KPURes18_MSF_loftr2(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr3':
-        model = PCReg_KPURes18_MSF_loftr3(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr4':
-        model = PCReg_KPURes18_MSF_loftr4(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr5':
-        model = PCReg_KPURes18_MSF_loftr5(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr6':
-        model = PCReg_KPURes18_MSF_loftr6(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr7':
-        model = PCReg_KPURes18_MSF_loftr7(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr8':
-        model = PCReg_KPURes18_MSF_loftr8(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr9':
-        model = PCReg_KPURes18_MSF_loftr9(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_loftr10':
-        model = PCReg_KPURes18_MSF_loftr10(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_mamba':
-        model = PCReg_KPURes18_MSF_mamba(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_redc':
-        model = PCReg_KPURes18_MSF_redc(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_gau0':
-        model = PCReg_KPURes18_MSF_gau0(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_gau1':
-        model = PCReg_KPURes18_MSF_gau1(cfg)
-    elif cfg.model == 'PCReg_KPURes18_MSF_mca0':
-        model = PCReg_KPURes18_MSF_mca0(cfg)    
+    elif cfg.model == 'FFMT':
+        model = FFMT(cfg)
+    elif cfg.model == 'FFMT1':
+        model = FFMT1(cfg)
+    elif cfg.model == 'FFMT2':
+        model = FFMT2(cfg)
+    elif cfg.model == 'FFMT3':
+        model = FFMT3(cfg)
+    elif cfg.model == 'FFMT4':
+        model = FFMT4(cfg)
+    elif cfg.model == 'FFMT5':
+        model = FFMT5(cfg)
+    elif cfg.model == 'FFMT6':
+        model = FFMT6(cfg)
+    elif cfg.model == 'FFMT7':
+        model = FFMT7(cfg)
+    elif cfg.model == 'FFMT8':
+        model = FFMT8(cfg)
+    elif cfg.model == 'FFMT9':
+        model = FFMT9(cfg)
+    elif cfg.model == 'FFMT10':
+        model = FFMT10(cfg)
+    elif cfg.model == 'FFMT_mamba':
+        model = FFMT_mamba(cfg)
+    elif cfg.model == 'FFMT_redc':
+        model = FFMT_redc(cfg)
+    elif cfg.model == 'FFMT_gau0':
+        model = FFMT_gau0(cfg)
+    elif cfg.model == 'FFMT_gau1':
+        model = FFMT_gau1(cfg)
+    elif cfg.model == 'FFMT_mca0':
+        model = FFMT_mca0(cfg)    
     
     else:
         raise ValueError("Model {} is not recognized.".format(cfg.name))
